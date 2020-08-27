0b3b9fc7c6d9ee5336d3e4220d93702933e9c91c
Chengheng Liang <liangchengheng1@gmail.com>
2020-08-26 21:46:04 -0500

    movement setup

---
+++ Assets/Scripts/PlayerCharacterAnimator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerCharacterAnimator : MonoBehaviour
+{
+    [SerializeField] ThirdPersonMovement _thirdpersonmovement = null;
+    const string IdleState = "Idle";
+    const string RunState = "Run";
+    const string JumpState = "Jumping";
+    const string FallState = "Falling";
+
+    Animator _animator = null;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+
+    }
+
+    public void OnIdle()
+    {
+        _animator.CrossFadeInFixedTime(IdleState, .2f);
+    }
+
+    public void OnStartRunning()
+    {
+        _animator.CrossFadeInFixedTime(RunState, .2f);
+    }
+
+    public void OnStartJumping()
+    {
+        _animator.CrossFadeInFixedTime(JumpState, .2f);
+    }
+
+    public void OnStartFalling()
+    {
+        _animator.CrossFadeInFixedTime(FallState, .2f);
+    }
+
+    private void OnEnable()
+    {
+        _thirdpersonmovement.Idle += OnIdle;
+        _thirdpersonmovement.StartRunning += OnStartRunning;
+    }
+
+    private void OnDisable()
+    {
+        _thirdpersonmovement.Idle -= OnIdle;
+        _thirdpersonmovement.StartRunning -= OnStartRunning;
+    }
+
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public event Action Idle = delegate { };
+    public event Action StartRunning = delegate { };
+    public event Action StartJumping = delegate { };
+    public event Action StartFalling = delegate { };
+
+    [SerializeField] CharacterController controller;
+    [SerializeField] Transform cam;
+
+    [SerializeField] float _speed = 6f;
+    [SerializeField] float _turnSmoothTime = .01f;
+
+    float _turnSmoothVelocity;
+    bool _isMoving = false;
+
+    private void Start()
+    {
+        Idle?.Invoke();
+    }
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        float upward = Input.GetAxisRaw("Jump");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            CheckIfStartedMoving();
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * _speed * Time.deltaTime);
+        }
+        else
+        {
+            CheckIfStoppedMoving();
+        }
+    }
+
+    private void CheckIfStartedMoving()
+    {
+        if(_isMoving == false)
+        {
+            StartRunning?.Invoke();
+            Debug.Log("Started");
+        }
+        _isMoving = true;
+    }
+
+    private void CheckIfStoppedMoving()
+    {
+        if(_isMoving == true)
+        {
+            Idle?.Invoke();
+            Debug.Log("Stopped");
+        }
+        _isMoving = false; 
+    }
+
+}
